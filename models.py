b37f7c0693a3e831838157652ac3b549e528d9f9
Yehoon Jang <jangyh0420@gmail.com>
2022-03-10 19:34:30 +0900

    vnmt

--- models.py
+++ models.py
@@ -1,5 +1,7 @@
 import torch
 from torch import nn
+from torch.autograd import Variable
+import numpy as np
 
 
 # Device Setting
@@ -10,20 +12,134 @@ print(f'Initializing Device: {device}')
 
 
 # TODO: encoder, decoder GRU 구조 파악하고 새로 짜
-class encoder(nn.Module):
-    def __init__(self):
-        super(encoder, self).__init__()
-        self.encoder = nn.GRU(input_size=, hidden_size=hidden_size, bidirectional=True)
+class GRUCell(nn.Module):
+    def __init__(self, input_size, hidden_size, bias=True):
+        super(GRUCell, self).__init__()
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.bias = bias
 
-     def forward(self, x):
-        out, hidden = self.encoder(x)
+        self.x2h = nn.Linear(input_size, 3 * hidden_size, bias=bias)
+        self.h2h = nn.Linear(hidden_size, 3 * hidden_size, bias=bias)
+
+        self.reset_parameters()
+
+
+    def reset_parameters(self):
+        std = 1.0 / np.sqrt(self.hidden_size)
+        for w in self.parameters():
+            w.data.uniform_(-std, std)
+
+    def forward(self, input, hx=None):
+        # Inputs:
+        #       input: of shape (batch_size, input_size)
+        #       hx: of shape (batch_size, hidden_size)
+        # Output:
+        #       hy: of shape (batch_size, hidden_size)
+        if hx is None:
+            hx = Variable(input.new_zeros(input.size(0), self.hidden_size))
+
+        x_t = self.x2h(input)
+        h_t = self.h2h(hx)
+
+
+        x_reset, x_upd, x_new = x_t.chunk(3, 1)
+        h_reset, h_upd, h_new = h_t.chunk(3, 1)
+
+        reset_gate = torch.sigmoid(x_reset + h_reset)
+        update_gate = torch.sigmoid(x_upd + h_upd)
+        new_gate = torch.tanh(x_new + (reset_gate * h_new))
+
+        hy = update_gate * hx + (1 - update_gate) * new_gate
+
+        return hy
+
+
+class GRU(nn.Module):
+    def __init__(self, input_size, hidden_size, num_layers, bias, output_size):
+        super(GRU, self).__init__()
+
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+        self.bias = bias
+        self.output_size = output_size
+
+        self.rnn_cell_list = nn.ModuleList()
+
+        self.rnn_cell_list.append(GRUCell(self.input_size,
+                                          self.hidden_size,
+                                          self.bias))
+        for l in range(1, self.num_layers):
+            self.rnn_cell_list.append(GRUCell(self.hidden_size,
+                                              self.hidden_size,
+                                              self.bias))
+        self.fc = nn.Linear(self.hidden_size, self.output_size)
+
+
+    def forward(self, input, hx=None):
+        # Input of shape (batch_size, seqence length, input_size)
+        #
+        # Output of shape (batch_size, output_size)
+
+        if hx is None:
+            if torch.cuda.is_available():
+                h0 = Variable(torch.zeros(self.num_layers, input.size(0), self.hidden_size).cuda())
+            else:
+                h0 = Variable(torch.zeros(self.num_layers, input.size(0), self.hidden_size))
+        else:
+             h0 = hx
+
+        outs = []
+        hidden = list()
+        for layer in range(self.num_layers):
+            hidden.append(h0[layer, :, :])
+
+        for t in range(input.size(1)):
+            for layer in range(self.num_layers):
+                if layer == 0:
+                    hidden_l = self.rnn_cell_list[layer](input[:, t, :], hidden[layer])
+                else:
+                    hidden_l = self.rnn_cell_list[layer](hidden[layer - 1],hidden[layer])
+                hidden[layer] = hidden_l
+
+                hidden[layer] = hidden_l
+
+            outs.append(hidden_l)
+
+        # Take only last time step. Modify for seq to seq
+        out = outs[-1].squeeze()
+        out = self.fc(out)
+
+        # TODO out, hidden size 확인
         return out, hidden
 
+class encoder(nn.Module):
+    def __init__(self, gru):
+        super(encoder, self).__init__()
 
-class decoder(nn.Module):
-    def __init__(self):
-        super(decoder, self).__init__()
-        self.decoder = nn.GRU(input_size=, hidden_size=hidden_size, bidirectional=True)
+        self.gru = gru
+
+    # TODO 여기 논문 읽고 한번 더 확인!!
+    # def get_att_weight(self, dec_output, enc_outputs):  # get attention weight one 'dec_output' with 'enc_outputs'
+    #     n_step = len(enc_outputs)
+    #     attn_scores = torch.zeros(n_step)  # attn_scores : [n_step]
+    #
+    #     for i in range(n_step):
+    #         attn_scores[i] = self.get_att_score(dec_output, enc_outputs[i])
+    #
+    #     # Normalize scores to weights in range 0 to 1
+    #     return F.softmax(attn_scores).view(1, 1, -1)
+    #
+    # def get_att_score(self, dec_output, enc_output):  # enc_outputs [batch_size, num_directions(=1) * n_hidden]
+    #     score = self.attn(enc_output)  # score : [batch_size, n_hidden]
+    #     return torch.dot(dec_output.view(-1), score.view(-1))  # inner product make scalar value
+
+    def forward(self, src):
+        output, hidden = self.gru(src)
+
+
+
 
 
 class VNMT(nn.Module):
